b6f1a642a00331547ec6a614cd5abaabcfbf98ec
jiogho <jjiogho@gmail.com>
2023-11-25 15:27:25 +0100

    integration des veiw habitats

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashbaordComponent } from './pages/dashbaord/dashbaord.component';
 import { AllUsersComponent } from './pages/all-users/all-users.component';
+import { CreateHabitatComponent } from './pages/create-habitat/create-habitat.component';
+import { ViewImageHabitatComponent } from './pages/view-image-habitat/view-image-habitat.component';
 
-const routes: Routes = [{ path: '', component: DashbaordComponent },{ path: 'all-users', component: AllUsersComponent }];
+const routes: Routes = [
+     { path: '', component: DashbaordComponent },
+     { path: 'all-users', component: AllUsersComponent },
+     { path: 'create-habitat', component: CreateHabitatComponent },
+     { path: 'view-image-habitat', component: ViewImageHabitatComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+     imports: [RouterModule.forChild(routes)],
+     exports: [RouterModule],
 })
 export class AdminRoutingModule {}
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashbaordComponent } from './pages/dashbaord/dashbaord.component';
 import { AllUsersComponent } from './pages/all-users/all-users.component';
+import { CreateHabitatComponent } from './pages/create-habitat/create-habitat.component';
+import { ViewImageHabitatComponent } from './pages/view-image-habitat/view-image-habitat.component';
 
 
 @NgModule({
   declarations: [
     DashbaordComponent,
-    AllUsersComponent
+    AllUsersComponent,
+    CreateHabitatComponent,
+    ViewImageHabitatComponent
   ],
   imports: [
     CommonModule,
