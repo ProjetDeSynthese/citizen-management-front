5100e5b76f4027b3d73396d29208b90393fb4b07
Manel Issa <issamanel45@gmail.com>
2024-01-04 16:30:30 +0100

    habita interfaces

--- src/app/admin/pages/add-citoyens/add-citoyens.component.ts
+++ src/app/admin/pages/add-citoyens/add-citoyens.component.ts
@@ -1,4 +1,9 @@
+import { Citoyen } from './../../../interfaces/citoyen';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Habitat } from 'src/app/interfaces/habitat';
+import { CitoyenService } from 'src/app/services/citoyen.service';
+import { HabitatService } from 'src/app/services/habitat.service';
 
 @Component({
   selector: 'app-add-citoyens',
@@ -6,8 +11,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-citoyens.component.scss']
 })
 export class AddCitoyensComponent implements OnInit {
+
+
+  form!: FormGroup
+  habitats!: Habitat[]
+
+  constructor(private citoyenService: CitoyenService, private habitatService: HabitatService) {
+
+  }
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.onForm()
+    this.getAllHabitats()
+  }
+  getAllHabitats() {
+
+    this.habitatService.findAll().subscribe({
+      next: res => {
+        this.habitats = res
+      }
+    })
+
+  }
+
+  submit() {
+    if (this.form) {
+
+      var citoyen: Citoyen = {
+        name: this.form.value.name,
+        prenom: this.form.value.prenom,
+        sexe: this.form.value.sexe,
+        email: this.form.value.email,
+        situationMat: this.form.value.situationMat,
+        phone: this.form.value.phone,
+        nationalite: this.form.value.nationalite,
+        profession: this.form.value.profession,
+        numCni: this.form.value.numCni,
+
+      }
+
+
+      console.log(citoyen)
+
+    }
+  }
+
+  onForm() {
+    this.form = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      prenom: new FormControl('', [Validators.required]),
+      sexe: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      situationMat: new FormControl('', [Validators.required]),
+      phone: new FormControl('', [Validators.required]),
+      nationalite: new FormControl('', [Validators.required]),
+      profession: new FormControl('', [Validators.required]),
+      numCni: new FormControl('', [Validators.required]),
+      // habitat: new FormControl('', [Validators.required]),
+
+    })
   }
 
 }
--- src/app/interfaces/habitat.ts
+++ src/app/interfaces/habitat.ts
@@ -1,2 +1,12 @@
+import { Secteur } from "./secteur";
+
 export interface Habitat {
+
+      id ?: String,
+      description :String,
+      proprietaire : String,
+      latitude : DoubleRange,
+      longitude :DoubleRange,
+      secteur : Secteur,
+      image: String []
 }
